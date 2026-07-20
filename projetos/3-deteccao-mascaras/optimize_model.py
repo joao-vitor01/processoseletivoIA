cbaaccef9adcd2374e6218278ac2cb978beb2e7e
João Vitor <joaovitorlopesmiranda107@gmail.com>
2026-07-20 22:28:17 +0000

    WIP: adiciona scripts de treinamento e otimização TFLite

--- projetos/3-deteccao-mascaras/optimize_model.py
+++ projetos/3-deteccao-mascaras/optimize_model.py
@@ -8,6 +8,15 @@ from ultralytics import YOLO
 #   2. Exportar para TensorFlow Lite via model.export(format="tflite")
 #      (a Ultralytics gera automaticamente "model.tflite" na mesma pasta)
 # ---------------------------------------------------------------------------
+print ("Carregaando o modelo treinado (model.pt)")
+model = YOLO("model.pt")
+
+print ("Exportando para TensorFlow Lite")
+
+caminho_exportado = model.export(format="tflite", imgsz = 640)
+
+print("Arquivo está pronto na raiz do programa.")
+
 
 # insira seu código aqui
 
