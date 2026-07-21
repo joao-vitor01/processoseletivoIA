e97f07bcf0a9fe44a90bc3bb15469f5958a574a9
João Vitor <joaovitorlopesmiranda107@gmail.com>
2026-07-21 02:03:50 +0000

    Entrega do desafio técnico - João Vitor Lopes Miranda

--- projetos/3-deteccao-mascaras/optimize_model.py
+++ projetos/3-deteccao-mascaras/optimize_model.py
@@ -10,29 +10,30 @@ from ultralytics import YOLO
 #      (a Ultralytics gera automaticamente "model.tflite" na mesma pasta)
 # ---------------------------------------------------------------------------
 def main():
-    print ("Carregando o modelo treinado (model.pt)")
+    print("Carregando o modelo treinado (model.pt)")
     model_path = "model.pt"
 
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"O arquivo {model_path} não foi encontrado na raiz do projeto.")
+    
     model = YOLO(model_path)
     print("Iniciando processo de exportação e otimização")
 
     try:
-        caminho_exportado = model.export(format="tflite", imgsz = 640)
+        caminho_exportado = model.export(format="tflite", imgsz=640)
         print(f"Sucesso na exportação TFLite: {caminho_exportado}")
+        
     except Exception as e:
         print(f"\nFalha na exportação nativa: {e} ")
+        print("Tentando via ONNX")
+        try:
+            model.export(format="onnx", imgsz=640)
+        except Exception as e_onnx:
+            print(f"Erro crítico na exportação: {e_onnx}")
+            raise e_onnx
+            
+    finally:
+        os.system("pip install litert-torch ai-edge-litert torchvision")
 
-        if os.path.exists("model.tflite"):
-            print("Artefato 'model.tflite' valido!")
-        else:
-            print("Tentando via ONNX")
-            try:
-                model.export(format="onnx", imgsz=640)
-            except Exception as e_onnx:
-                print(f"Erro crítico na exportação: {e_onnx}")
-                raise e_onnx
 if __name__ == "__main__":
     main()
-
